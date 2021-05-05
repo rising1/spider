7fbcd2625b3a1c7fc07b98e37f6bfd1f677b83b1
rising1 <phfrost@btconnect.com>
2021-05-05 20:57:28 +0100

    mod2

--- spider.py
+++ spider.py
@@ -1,6 +1,7 @@
 import socket
 
 for x in range(255):
+    for y in range(255):
  #   print(socket.gethostbyaddr("2a00:1450:4009:802::2004"))
  #   print(socket.gethostbyaddr("86.153.207." + str(x)))
-    print(socket.gethostbyaddr("86."+ str(x)+ "." + str(x)+ "." + str(x)))
+        print(socket.gethostbyaddr("85."+ str(x)+ "." + str(x)+ "." + str(y)))
