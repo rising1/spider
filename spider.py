1dc247f17f72b449a3aaefe7c839653591eb9178
rising1 <phfrost@btconnect.com>
2021-05-06 20:36:22 +0100

    mod2

--- spider.py
+++ spider.py
@@ -1,7 +1,11 @@
 import socket
+import os.path
+
+if os.path.isfile("")
 
 for x in range(255):
     for y in range(255):
+        for z in range(255):
  #   print(socket.gethostbyaddr("2a00:1450:4009:802::2004"))
  #   print(socket.gethostbyaddr("86.153.207." + str(x)))
-        print(socket.gethostbyaddr("85."+ str(x)+ "." + str(x)+ "." + str(y)))
+            print(socket.gethostbyaddr("85."+ str(x)+ "." + str(y)+ "." + str(z)))
