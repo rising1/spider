3a1669d2732a9ea02fc8db7032f5834db7bca295
rising1 <phfrost@btconnect.com>
2021-05-06 21:18:26 +0100

    mod2

--- spider.py
+++ spider.py
@@ -3,13 +3,16 @@ import socket
 f= open("C:/Users/phfro/PycharmProjects/spider/results.txt","w+")
 
 
-for x in range(10,255):
-    for y in range(10,255):
-        for z in range(10,255):
+for x in range(86,255):
+    for y in range(153,255):
+        for z in range(207,255):
             for k in range(10,255):
  #   print(socket.gethostbyaddr("2a00:1450:4009:802::2004"))
  #   print(socket.gethostbyaddr("86.153.207." + str(x)))
-                print(socket.gethostbyaddr(str(x)+ "." + str(y)+ "." + str(z) + str(k)))
-                f.write(str(x)+ "." + str(y)+ "." + str(z) + str(k) +"," +
-                    socket.gethostbyaddr(str(x)+ "." + str(y)+ "." + str(z) + str(k)))
+                try:
+                    print(socket.gethostbyaddr(str(x) + "." + str(y) + "." + str(z) + "." + str(k)))
+                    f.write(str(x)+ "." + str(y)+ "." + str(z) + "." + str(k) + " , " +
+                    socket.gethostbyaddr(str(x) + "." + str(y) + "." + str(z) + "." + str(k)))
+                except:
+                    print("host not resolved " + str(x) + "." + str(y) + "." + str(z) + "." + str(k))
 f.close()
