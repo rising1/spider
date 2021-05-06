8c1b0564a3b0fb34783bc4739acda44b205fb579
rising1 <phfrost@btconnect.com>
2021-05-06 21:04:55 +0100

    mod2

--- spider.py
+++ spider.py
@@ -1,11 +1,15 @@
 import socket
-import os.path
 
-if os.path.isfile("")
+f= open("C:/Users/phfro/PycharmProjects/spider/results.txt","w+")
 
-for x in range(255):
-    for y in range(255):
-        for z in range(255):
+
+for x in range(10,255):
+    for y in range(10,255):
+        for z in range(10,255):
+            for k in range(10,255):
  #   print(socket.gethostbyaddr("2a00:1450:4009:802::2004"))
  #   print(socket.gethostbyaddr("86.153.207." + str(x)))
-            print(socket.gethostbyaddr("85."+ str(x)+ "." + str(y)+ "." + str(z)))
+                print(socket.gethostbyaddr(str(x)+ "." + str(y)+ "." + str(z) + str(k)))
+                f.write(str(x)+ "." + str(y)+ "." + str(z) + str(k) +"," +
+                    socket.gethostbyaddr(str(x)+ "." + str(y)+ "." + str(z) + str(k)))
+f.close()
