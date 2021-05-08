4cbf9c8703dbb3fe222b2b565c701d37437b54f2
rising1 <phfrost@btconnect.com>
2021-05-08 16:56:01 +0100

    mod2

--- spider.py
+++ spider.py
@@ -2,6 +2,17 @@ import socket
 
 f= open("C:/Users/phfro/PycharmProjects/spider/results.txt","w+")
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#target = input('What website to scan?: ')
+
+def pscan(port):
+    try:
+        con = s.connect((target[0],port))
+        return True
+    except:
+        return False
+
 
 for x in range(86,255):
     for y in range(153,255):
@@ -10,9 +21,15 @@ for x in range(86,255):
  #   print(socket.gethostbyaddr("2a00:1450:4009:802::2004"))
  #   print(socket.gethostbyaddr("86.153.207." + str(x)))
                 try:
-                    print(socket.gethostbyaddr(str(x) + "." + str(y) + "." + str(z) + "." + str(k)))
-                    f.write(str(x)+ "." + str(y)+ "." + str(z) + "." + str(k) + " , " +
-                    socket.gethostbyaddr(str(x) + "." + str(y) + "." + str(z) + "." + str(k)))
-                except:
-                    print("host not resolved " + str(x) + "." + str(y) + "." + str(z) + "." + str(k))
+                    target = socket.gethostbyaddr(str(x) + "." + str(y) + "." + str(z) + "." + str(k))
+                    result = " placeholder "
+                    #for x in range(80, 80):
+                    #    if pscan(x):
+                    #        result = ('Port', x, 'is open')
+                    #    else:
+                    #        result = ('Port', x, 'is closed')
+                    print(target[0] + " " + result)
+                    f.write(target[0] + " " + result +  "\n")
+                except Exception as e:
+                    print(str(e) + " " + str(x) + "." + str(y) + "." + str(z) + "." + str(k))
 f.close()
